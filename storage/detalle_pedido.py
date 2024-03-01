045764c0cad02bdbe932d68137869dcee98767e3
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-03-01 08:36:11 -0500

    proyecto Jardineria, data

---
+++ storage/detalle_pedido.py
@@ -0,0 +1,9 @@
+detalle_pedido = [
+    [
+        "codigo_producto": 0,
+        "codigo_producto": "",
+        "cantidad": 0,
+        "preio_unidad": 1.0,
+        "numerp_linea": 0
+    ]
+]
---
+++ storage/prodycto.py
@@ -0,0 +1,13 @@
+prodycto = [
+    {
+        "codigo_producto": "",
+        "nombre": "",
+        "gama": "",
+        "dimensiones": "",
+        "proovedor": "",
+        "descripcion": "",
+        "cantidad_en_stock": 0,
+        "precio_venta": 1.0,
+        "precio_de_venta": 1.0
+    }
+]
