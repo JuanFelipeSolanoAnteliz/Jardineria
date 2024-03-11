b6253065b84b369838c4c838ceacb31a689914a6
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com.>
2024-03-10 22:10:55 -0500

    se crearon dos nuevos filtros y el indice del modulo "pago"

--- modulos/getPago.py
+++ modulos/getPago.py
@@ -13,3 +13,35 @@ def getAllPago08Paypal():
                 })
     return formaPago
 
+def getAllpagosPaypal():
+    paypal = []
+    for val in pa.pagos:
+        if val.get("forma_pago") == "paypal":
+            datapagos = {
+
+            "codigo": val.get("codigo_cliente"),
+            "forma_pago": val.get("forma_pago"),
+            "id_transaccion": val.get("id_transaccion"),
+            "fecha": val.get("fecha_pago"),
+            "total": val.get("total")
+            } 
+    return paypal.append(datapagos)
+
+def getAllFormaPago():
+    formaDePago =[]
+    formaPagoRep = set()
+    for val in pa.pagos:
+        if val.get("forma_pago") not in formaPagoRep:
+            formaDePago.append({"forma_pago": val.get("forma_pago")})
+            formaPagoRep.add(val.get("forma_pago"))
+            
+    return formaDePago
+
+def menu():
+    print(""" 
+                            Reporte de pagos
+          1. lista de pagos realizados con paypal en 2008
+          2. lista de pagos realizados con paypal 
+          3. obtener una lista de formas de pagos
+""")
+
